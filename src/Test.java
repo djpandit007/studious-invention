e6b1b3d4e9862aa45034a7131aa3ab7030207d29
Dhananjay <dvpandit@asu.edu>
2016-11-23 12:31:16 -0700

    Return one word from DB

--- src/Test.java
+++ src/Test.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 public class Test {
 	private Words words;
 	private ArrayList<ArrayList<String>> wordsDB;
-	private ArrayList<String> oneWord;
+	
 
 	public Test() throws FileNotFoundException, IOException
 	{
 		words = new Words();
 		wordsDB = words.getList();
-		oneWord = words.getOne();
+		
 	}
 	
 	public static void main(String args[]) throws FileNotFoundException, IOException
 	{
 		Test test = new Test();
 		System.out.println(test.wordsDB);
-		System.out.println(test.oneWord);
+		
 	}
 
 }
